b96f9b6f071ce6534f176b3d7e6d41c15396106e
mlpeschl <peschl@protonmail.com>
2020-04-17 23:49:25 +0200

    Added labels

--- Markus/correlation.py
+++ Markus/correlation.py
@@ -8,5 +8,7 @@ data = np.load('Datares/tensor_daily_mean_5D.npy')
 X = data_to_xarray(data)
 X.head()
 
-#Managed to do it in one line
-ax = sns.heatmap(np.corrcoef(X.sel(station='Harlingen Havenmond West',exp='rcp45',model='CNRM-CERFACS-CNRM-CM5'), rowvar = False),annot=True)
+variables = ["rsds","tas","uas","vas","clt","hurs","ps"]
+corr = np.corrcoef(X.sel(station='Harlingen Havenmond West',exp='rcp45',model='CNRM-CERFACS-CNRM-CM5'),rowvar = False)
+
+ax = sns.heatmap(corr,annot=True, xticklabels = variables, yticklabels = variables)
