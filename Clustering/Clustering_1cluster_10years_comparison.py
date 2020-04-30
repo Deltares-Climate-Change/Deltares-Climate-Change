9734401d1d16eaa1aa7a348190cb68613d058256
Simon <simon.vano@xs4all.nl>
2020-04-30 14:05:50 +0200

    Clustering friday 2 pm

--- Clustering/Clustering_1cluster_10years_comparison.py
+++ Clustering/Clustering_1cluster_10years_comparison.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Apr 23 09:56:56 2020
+
 @author: simon
 """
 import datetime
@@ -9,17 +10,17 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_samples, silhouette_score
 
-def MonthCounter(Labels,n_clusters):
+def MonthCounter(Labels,n_clusters,StartTime):
     Dat = [[] for i in range(n_clusters)]
-    Dag = datetime.date(2006,1,1)
+    Dag = StartTime
     for i in range(len(Labels)):
         Dat[Labels[i]].append(Dag.month)
         Dag += datetime.timedelta(days = 1)
     return(Dat)
     
-def YearCounter(Labels,n_clusters):
+def YearCounter(Labels,n_clusters,StartTime):
     Dat = [[] for i in range(n_clusters)]
-    Dag = datetime.date(2006,1,1)
+    Dag = StartTime
     for i in range(len(Labels)):
         Dat[Labels[i]].append(Dag.year)
         Dag += datetime.timedelta(days = 1)
@@ -74,50 +75,46 @@ SubDataStation = SubData[:,:,st] #Select the station that we are going to analys
 range_n_clusters = [3]
 
 for n_clusters in range_n_clusters:
-    clusterer = KMeans(n_clusters=n_clusters, random_state=10).fit(SubDataStation)
+    data1 = SubDataStation
+    clusterer = KMeans(n_clusters=n_clusters, random_state=10).fit(data1)
     cluster_labels = clusterer.labels_
-    silhouette_avg = silhouette_score(SubDataStation, cluster_labels)
-    
-    
-    
+    silhouette_avg = silhouette_score(data1, cluster_labels)   
     print("For n_clusters =", n_clusters, 
           "The average silhouette_score is :", silhouette_avg)
-    sample_silhouette_values = silhouette_samples(SubDataStation, cluster_labels)
-   
-    
-cluster_labels_array = np.array(cluster_labels)
-n_in_clusters = []
-n_in_clusteri = 0
-for i in range(range_n_clusters[0]):
-    n_in_clusteri = len(np.where(cluster_labels_array == i)[0])
-    n_in_clusters.append(n_in_clusteri)
-    print('The number of datapoints in cluster '+str(i)+' is: '+str(n_in_clusteri))
-    
+    sample_silhouette_values = silhouette_samples(data1, cluster_labels)
 
-Month_Counter = MonthCounter(cluster_labels,n_clusters)
-Year_Counter = YearCounter(cluster_labels,n_clusters)
+cluster_labels1 = cluster_labels[:3650]
+cluster_labels2 = cluster_labels[-3650:]
+
+Start1 = datetime.date(2006,1,1)
+Start2 = datetime.date(2086,1,1)
+
+Month_Counter = MonthCounter(cluster_labels,n_clusters,Start1)
+Year_Counter = YearCounter(cluster_labels,n_clusters,Start1)
+
+Month_Counter1 = MonthCounter(cluster_labels1,n_clusters,Start1)
+Year_Counter1 = YearCounter(cluster_labels1,n_clusters,Start1)
+
+Year_Counter2 = YearCounter(cluster_labels2,n_clusters,Start2)
+Month_Counter2 = MonthCounter(cluster_labels2,n_clusters,Start2)
 
 fig, axes = plt.subplots(nrows=2, ncols=2)
 ax0, ax1, ax2, ax3 = axes.flatten()
 
 
 ax0.hist(Month_Counter, 12, density=True, histtype='bar')
-ax0.legend(prop={'size': 10})
 ax0.set_title('Divide in months')
+
+ax2.hist(Month_Counter1+Month_Counter2, 12, density=True, histtype='bar')
+ax2.set_title('Divide in months')
 
 
 ax1.hist(Year_Counter, 10, density=True, histtype='bar')
-ax1.legend(prop={'size': 10})
 ax1.set_title('Divide in years')
 
+ax3.hist(Year_Counter1+Year_Counter2, 10, density=True, histtype='bar')
+ax3.set_title('Divide in years')
 
-for var in range(7):
-#var = 0
-    M = []
-    for cl in range(n_clusters):
-        INDX = np.where(cluster_labels_array == cl)
-        M.append(Data[INDX,var,st, mdl, exp])
-    fig, axs = plt.subplots(7, 1)
-    axs.boxplot(M)
 
-plt.show()
+#ax5.legend(prop={'size': 10})
+
