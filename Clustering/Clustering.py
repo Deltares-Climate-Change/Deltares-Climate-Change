357d3c7a6d1d4c2679364ddf3372df7538e57eaa
Simon <simon.vano@xs4all.nl>
2020-05-24 09:56:34 +0200

    Cleaning up the code

--- Clustering/Clustering.py
+++ Clustering/Clustering.py
@@ -2,6 +2,12 @@
 """
 Created on Thu Apr 23 09:56:56 2020
 @author: simon
+This is the simplest version of the clustering files. 
+One can select the desired station, model, and experiment, and the number of clusters.
+The script will output the average silhouette score for each number of clusters, the size
+of each cluster, and show the different clusters in a histogram where one can 
+see how the clusters are spread during the year as well as the spread over the years.
+
 """
 import datetime
 import numpy as np
@@ -10,6 +16,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_samples, silhouette_score
 
 def MonthCounter(Labels,n_clusters):
+    "For each cluster, determine in which month the days are located"
     Dat = [[] for i in range(n_clusters)]
     Dag = datetime.date(2006,1,1)
     for i in range(len(Labels)):
@@ -18,6 +25,7 @@ def MonthCounter(Labels,n_clusters):
     return(Dat)
     
 def YearCounter(Labels,n_clusters):
+    "For each cluster, determine in which year the days are located"
     Dat = [[] for i in range(n_clusters)]
     Dag = datetime.date(2006,1,1)
     for i in range(len(Labels)):
@@ -25,10 +33,15 @@ def YearCounter(Labels,n_clusters):
         Dag += datetime.timedelta(days = 1)
     return(Dat)
 
+
+"Load the preprocessed data from the file placed in Datares"
 Data = np.load('../Datares/tensor_daily_mean_5D.npy')
 NanINDX = np.argwhere(np.isnan(Data))
 for i in range(len(NanINDX)):
     Data[NanINDX[i][0],NanINDX[i][1],NanINDX[i][2],NanINDX[i][3],NanINDX[i][4]] = 200
+    
+
+"Determine which station, model, and experiment will be analyzed" 
 st = 0
 STATIONS = ['Marsdiep Noord','Doove Balg West',
                 'Vliestroom','Doove Balg Oost',
@@ -61,18 +74,17 @@ MODELS = ['CNRM-CERFACS-CNRM-CM5','ICHEC-EC-EARTH',
 exp = 1
 EXPERIMENTS = ['rcp45','rcp85']
 
+
+"Select the appropriate data, and normalize the variables
 SubData = Data[:,:,:,mdl,exp]
-
-std = 1
-
 for i in range(SubData.shape[1]):
     SubData[:,i,:] = SubData[:,i,:]-SubData[:,i,:].mean()
-    SubData[:,i,:] = SubData[:,i,:]/(SubData[:,i,:].std(ddof = std))
+    SubData[:,i,:] = SubData[:,i,:]/(SubData[:,i,:].std(ddof = 1))
+SubDataStation = SubData[:,:,st] 
 
-SubDataStation = SubData[:,:,st] #Select the station that we are going to analyse
 
-range_n_clusters = [2,3,4,5,6,7,8,9,10,11,12]
-
+"Perform the clustering
+range_n_clusters = [2,3]
 for n_clusters in range_n_clusters:
     clusterer = KMeans(n_clusters=n_clusters, random_state=10).fit(SubDataStation)
     cluster_labels = clusterer.labels_
@@ -80,40 +92,32 @@ for n_clusters in range_n_clusters:
     print("For n_clusters =", n_clusters, 
           "The average silhouette_score is :", silhouette_avg)
    
+   
+"Analyze the clusters and plot the results 
+ cluster_labels_array = np.array(cluster_labels)
+ n_in_clusters = []
+ n_in_clusteri = 0
+ for i in range(range_n_clusters[0]):
+     n_in_clusteri = len(np.where(cluster_labels_array == i)[0])
+     n_in_clusters.append(n_in_clusteri)
+     print('The number of datapoints in cluster '+str(i)+' is: '+str(n_in_clusteri))
     
-# cluster_labels_array = np.array(cluster_labels)
-# n_in_clusters = []
-# n_in_clusteri = 0
-# for i in range(range_n_clusters[0]):
-#     n_in_clusteri = len(np.where(cluster_labels_array == i)[0])
-#     n_in_clusters.append(n_in_clusteri)
-#     print('The number of datapoints in cluster '+str(i)+' is: '+str(n_in_clusteri))
-    
 
-# Month_Counter = MonthCounter(cluster_labels,n_clusters)
-# Year_Counter = YearCounter(cluster_labels,n_clusters)
+ Month_Counter = MonthCounter(cluster_labels,n_clusters)
+ Year_Counter = YearCounter(cluster_labels,n_clusters)
 
-# fig, axes = plt.subplots(nrows=2, ncols=2)
-# ax0, ax1, ax2, ax3 = axes.flatten()
+ fig, axes = plt.subplots(nrows=2, ncols=2)
+ ax0, ax1, ax2, ax3 = axes.flatten()
 
 
-# ax0.hist(Month_Counter, 12, density=True, histtype='bar')
-# ax0.legend(prop={'size': 10})
-# ax0.set_title('Divide in months')
+ ax0.hist(Month_Counter, 12, density=True, histtype='bar')
+ ax0.legend(prop={'size': 10})
+ ax0.set_title('Divide in months')
 
 
-# ax1.hist(Year_Counter, 10, density=True, histtype='bar')
-# ax1.legend(prop={'size': 10})
-# ax1.set_title('Divide in years')
+ ax1.hist(Year_Counter, 10, density=True, histtype='bar')
+ ax1.legend(prop={'size': 10})
+ ax1.set_title('Divide in years')
 
 
-# for var in range(7):
-# #var = 0
-#     M = []
-#     for cl in range(n_clusters):
-#         INDX = np.where(cluster_labels_array == cl)
-#         M.append(Data[INDX,var,st, mdl, exp])
-#     fig, axs = plt.subplots(7, 1)
-#     axs.boxplot(M)
-
-# plt.show()
+ plt.show()
